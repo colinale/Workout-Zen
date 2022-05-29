c9338c7060ed93b121da80c9a4f5192e68d299e7
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-05-29 18:09:08 -0400

    created dynamic exercises page that will change out info based on what exercise the user pics

---
+++ assets/js/exercises.js
@@ -0,0 +1,9 @@
+var videoLinks = [];
+
+//array with objects having title, description, video, image etc
+console.log(localStorage);
+
+//grab info from local storage
+//create variables that have the information specific to crunches, planks and flutter kicks
+//put it in an array??
+//append to exercises page
--- assets/js/workout.js
+++ assets/js/workout.js
@@ -3,6 +3,7 @@ var muscleEl = document.querySelector("#target-btn");
 var exerciseButtonsEl = document.querySelector("#exercise-buttons");
 var exerciseListUlEl = document.querySelector(".exercise-list");
 var alertDivEl = document.querySelector(".alert-div");
+var templateEL = $("#exercise-template");
 
 //this function handles the user clicking each muscle button
 var buttonClickHandler = function (event) {
@@ -25,6 +26,7 @@ var exerciseListAbs = function () {
     // request was successful
     if (response.ok) {
       response.json().then(function (data) {
+        console.log(data);
         var crunches = data.results[41];
         var plank = data.results[141];
         var flutterKicks = data.results[67];
@@ -34,7 +36,13 @@ var exerciseListAbs = function () {
         //display each exercise name
         for (var i = 0; i < absExerciseArray.length; i++) {
           var absExerciseLiEl = document.createElement("li");
-          absExerciseLiEl.innerHTML = "<a href ='./crunches.html'>" + absExerciseArray[i].name + "</a>";
+          absExerciseLiEl.innerHTML = "<a href ='./exercises.html'>" + absExerciseArray[i].name + "</a>";
+          // console.log(absExerciseArray[i].name);
+          absExerciseLiEl.addEventListener("click", function (evt) {
+            console.log(evt.target.innerHTML);
+            var exerciseName = absExerciseArray[i].name;
+            localStorage.setItem("exercises", exerciseName);
+          });
           exerciseListUlEl.appendChild(absExerciseLiEl);
         }
       });
