a880e57fbd5134372fc6477bc8138d7dc67ea1d7
Amanda Klenk <amandamklenk3@gmail.com>
2022-05-30 10:28:27 -0400

    set up divs and beginning object in exercise.js

--- assets/js/exercises.js
+++ assets/js/exercises.js
@@ -1,8 +1,27 @@
-var videoLinks = [];
+//targeted divs and array for video links
+var videoLinks = [{'Crunches': '<iframe width="560" height="315" src="https://www.youtube.com/embed/Xyd_fa5zoEU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+{'Plank': '<iframe width="560" height="315" src="https://www.youtube.com/embed/BQu26ABuVS0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'}, {'Flutter Kicks': '<iframe width="560" height="315" src="https://www.youtube.com/embed/BQu26ABuVS0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'}];
+var exerciseTitleEl = document.getElementById("title");
+var exerciseDescriptionEl = document.getElementById("description");
+var exerciseVideoDivEl = document.getElementById("video");
 
-//array with objects having title, description, video, image etc
-console.log(localStorage);
+var exerciseData = function(){
+    var exerciseApi = "https://wger.de/api/v2/exercise/?language=2&limit=200";
 
+    // make a get request to url
+    fetch(exerciseApi).then(function (response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function (data) {
+         console.log(data);
+        var exerciseName = document.
+        });
+    }
+  });
+};
+
+
+exerciseData();
 //grab info from local storage
 //create variables that have the information specific to crunches, planks and flutter kicks
 //put it in an array??
